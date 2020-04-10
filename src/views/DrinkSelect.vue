004085d24b154304ce4e00579286c1926e30c3be
Aidan Daniel <aidan.d@wearedrum.com>
2020-04-10 11:44:30 +0100

    Laying out the glass section & Playing around with animation

--- src/views/DrinkSelect.vue
+++ src/views/DrinkSelect.vue
@@ -1,14 +1,45 @@
 <template>
   <div class="drink-select">
     <p>Drink selected: {{currentDrink.name}}</p>
+    <div class="drink-wrapper">
+      <GlassOne/>
+      <div class="liquid" v-bind:style="{ height: liquidHeight + '%', maxWidth: liquidMaxWidth + 'px', maxHeight: liquidMaxHeight + 'px' }"></div>
+    </div>
+    <button v-on:click="testLiquid()" type="button" name="button">test</button>
   </div>
 </template>
 
 <script>
+import GlassOne from '@/assets/glass2.svg';
+
 export default {
   name: 'DrinkSelect',
+  components: {
+    GlassOne,
+  },
   props: {
     currentDrink: Object,
+  },
+  data() {
+    return {
+      liquidMaxWidth: 0,
+      liquidMaxHeight: 0,
+      liquidHeight: 0,
+    };
+  },
+  methods: {
+    testLiquid() {
+      console.log(this.liquidHeight);
+      if (this.liquidHeight === 0) {
+        this.liquidHeight = 30;
+      } else {
+        this.liquidHeight = 0;
+      }
+    },
+  },
+  mounted() {
+    this.liquidMaxHeight = document.getElementById('liquidFillArea').getBoundingClientRect().height;
+    this.liquidMaxWidth = document.getElementById('liquidFillArea').getBoundingClientRect().width + 10;
   },
 };
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,41 @@
 // vue.config.js
 module.exports = {
-    publicPath: '',
-    indexPath: "index.html",
-    outputDir: "www",
-    assetsDir: '',
-    css: {
-     loaderOptions: {
-       // pass options to sass-loader
-       // @/ is an alias to src/
-       // by default the `sass` option will apply to both syntaxes
-       // because `scss` syntax is also processed by sass-loader underlyingly
-       // but when configuring the `data` option
-       // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
-       // in that case, we can target the `scss` syntax separately using the `scss` option
-       scss: {
-         prependData: `@import "~@/assets/scss/main.scss";`
-       },
-     }
-   }
-}
+  publicPath: '',
+  indexPath: 'index.html',
+  outputDir: 'www',
+  assetsDir: '',
+  css: {
+    loaderOptions: {
+      // pass options to sass-loader
+      // @/ is an alias to src/
+      // by default the `sass` option will apply to both syntaxes
+      // because `scss` syntax is also processed by sass-loader underlyingly
+      // but when configuring the `data` option
+      // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
+      // in that case, we can target the `scss` syntax separately using the `scss` option
+      scss: {
+        prependData: '@import "~@/assets/scss/main.scss";',
+      },
+    },
+  },
+  chainWebpack: (config) => {
+    const svgRule = config.module.rule('svg');
+
+    svgRule.uses.clear();
+
+    svgRule
+      .use('vue-svg-loader')
+      .loader('vue-svg-loader')
+      .options({
+        svgo: {
+          plugins: [
+            { removeDoctype: true },
+            { removeComments: true },
+            { cleanupIDs: false },
+            { collapseGroups: false },
+            { removeEmptyContainers: false },
+          ],
+        },
+      });
+  },
+};
